b7ad65df663035a35dc8969cbf1e955a883ec4d1
Rainsho <djkyux@163.com>
2018-06-10 00:35:35 +0800

    koa server

---
+++ index.ts
@@ -0,0 +1,39 @@
+import connection from './src/connection';
+import Eto from './src/model';
+
+const Koa = require('koa');
+const router = require('koa-router');
+const serve = require('koa-static');
+const path = require('path');
+
+const app = new Koa();
+const src = router();
+const api = router({ prefix: '/api' });
+
+src.get('/', serve(path.join(__dirname, 'front-end')));
+
+api.get('/eto/date', async (ctx) => {
+  const date = await connection(Eto)((connection) =>
+    connection
+      .getRepository(Eto)
+      .createQueryBuilder()
+      .select('DISTINCT CREATETIME')
+      .getRawMany()
+  );
+  ctx.body = date;
+});
+
+api.get('/eto/:time', async (ctx) => {
+  const { time } = ctx.params;
+  const date = new Date(parseInt(time));
+  const etos = await connection(Eto)((connection) =>
+    connection.getRepository(Eto).find({ createtime: date })
+  );
+  ctx.body = etos;
+});
+
+app.use(src.routes());
+app.use(api.routes());
+
+app.listen(8888);
+console.log('listening on port 8888');
