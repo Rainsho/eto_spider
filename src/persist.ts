e36f6145687729e481e910f409c10f746218ac4c
Rainsho <djkyux@163.com>
2018-06-17 04:23:49 +0800

    fix te error

--- src/persist.ts
+++ src/persist.ts
@@ -2,10 +2,10 @@ import connection from './connection';
 import fetchData from './spider';
 import Eto from './model';
 
-connection(Eto)(async (connection) => {
+connection()(async connection => {
   const data = await fetchData();
   const date = new Date();
-  const etos = data.map((eto) => new Eto(eto.address, eto.percentage, date));
+  const etos = data.map(eto => new Eto(eto.address, eto.percentage, date));
 
   await connection.manager.save(etos);
   console.log('job done!');
