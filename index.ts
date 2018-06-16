02511b4e849144febf59f3b098eda817b4e789b0
Rainsho <djkyux@163.com>
2018-06-16 22:45:20 +0800

    add logger

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import connection from './src/connection';
-import Eto from './src/model';
+import { getDate, getEtos, logger } from './src/utils';
 
 const Koa = require('koa');
 const router = require('koa-router');
@@ -11,27 +10,10 @@ const src = router();
 const api = router({ prefix: '/api' });
 
 src.get('/', serve(path.join(__dirname, 'front-end')));
+api.get('/eto/date', getDate);
+api.get('/eto/:time', getEtos);
 
-api.get('/eto/date', async (ctx) => {
-  const date = await connection(Eto)((connection) =>
-    connection
-      .getRepository(Eto)
-      .createQueryBuilder()
-      .select('DISTINCT CREATETIME AS TIME')
-      .getRawMany()
-  );
-  ctx.body = date;
-});
-
-api.get('/eto/:time', async (ctx) => {
-  const { time } = ctx.params;
-  const date = new Date(parseInt(time));
-  const etos = await connection(Eto)((connection) =>
-    connection.getRepository(Eto).find({ createtime: date })
-  );
-  ctx.body = etos;
-});
-
+app.use(logger);
 app.use(src.routes());
 app.use(api.routes());
 
---
+++ src/utils.ts
@@ -0,0 +1,32 @@
+import connection from './connection';
+import Eto from './model';
+
+export async function getDate(ctx) {
+  const date = await connection(Eto)((connection) =>
+    connection
+      .getRepository(Eto)
+      .createQueryBuilder()
+      .select('DISTINCT CREATETIME AS TIME')
+      .getRawMany()
+  );
+  ctx.body = date;
+}
+
+export async function getEtos(ctx) {
+  const { time } = ctx.params;
+  const date = new Date(parseInt(time));
+  const etos = await connection(Eto)((connection) =>
+    connection.getRepository(Eto).find({ createtime: date })
+  );
+  ctx.body = etos;
+}
+
+export async function logger(ctx, next) {
+  const { ip, method, url } = ctx.request;
+  const start = new Date();
+  console.log(`=== ${start.toISOString()} from ${ip}`);
+  console.log(`--> ${method} ${url}`);
+  await next();
+  const { status, length } = ctx.response;
+  console.log(`<-- ${method} ${url} ${status} ${Date.now() - start.getTime()}ms ${length}`);
+}
